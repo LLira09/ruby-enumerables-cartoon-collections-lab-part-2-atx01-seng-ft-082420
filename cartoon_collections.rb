785442a65bc71954bfebe21eaa35b64f9a59325f
Jake <llira09@gmail.com>
2020-07-29 17:12:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,6 +16,6 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find do |elemtent|
+  planeteer_calls.find do |element|
     valid_calls.include? element
 end
